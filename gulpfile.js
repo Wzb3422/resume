8e48759501c645dc8912e398a3e858452a03826a
wzb <wzb3422@gmail.com>
2020-05-13 09:11:09 +0800

    :art: 调整高度

--- gulpfile.js
+++ gulpfile.js
@@ -133,7 +133,7 @@ gulp.task('pdf', ['set-pdf-port', 'default', 'webserver'], async () => {
   await page.pdf({
     path: exportPath,
     width: '9.64in',
-    height: '16.5in',
+    height: '15.9in',
     printBackground: true,
     displayHeaderFooter: false,
     margin: {
